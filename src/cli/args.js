6d803ad9e4703856c8894867a469dd8e52cf779a
Tuz <dmitry.tuz@awtg.co.uk>
2022-06-05 22:09:50 +0400

    feat: fill cli functions with body

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const {argv} = process;
+
+    argv.slice(2).forEach((val, index, array) => {
+        if(index%2!==1) console.log(`${val}: ${array[index+1]}`);
+    });
 };
+
+parseArgs();
