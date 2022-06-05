1431c1db5b99c19b9e8401eda89570ac905830be
Tuz <dmitry.tuz@awtg.co.uk>
2022-06-05 22:08:17 +0400

    feat: fill hash functions with body to get hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import {readFile} from "fs/promises";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    let crypto;
+
+    try {
+        crypto = await import('crypto');
+        const secret = 'secret';
+
+        const content = await readFile('files/fileToCalculateHashFor.txt');
+        const hash = crypto
+            .createHmac('sha256', secret)
+            .update(content.toString())
+            .digest('hex');
+
+        return hash;
+    } catch (e) {
+        throw new Error('operation failed');
+    }
 };
+
+calculateHash();
